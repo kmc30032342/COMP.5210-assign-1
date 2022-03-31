9a20a87b866890e8e713ac82fe3385be42dcfa38
kyla carter <kylacarter2002@gmail.com>
2022-03-31 15:10:44 +1300

    message

---
+++ scripts/myscript.js
@@ -0,0 +1,32 @@
+function Display_show_1()
+{
+    document.getElementById("show").style.display = "block";
+    document.getElementById("show_2").style.display = "none";
+    document.getElementById("show_3").style.display = "none";
+    document.getElementById("show_4").style.display = "none";
+    
+}
+
+function Display_show_2()
+{
+    document.getElementById("show_2").style.display = "block";
+    document.getElementById("show").style.display = "none";
+    document.getElementById("show_3").style.display = "none";
+    document.getElementById("show_4").style.display = "none";
+}
+
+function Display_show_3()
+{
+    document.getElementById("show_3").style.display = "block";
+    document.getElementById("show").style.display = "none";
+    document.getElementById("show_2").style.display = "none";
+    document.getElementById("show_4").style.display = "none";
+}
+
+function Display_show_4()
+{
+    document.getElementById("show_4").style.display = "block";
+    document.getElementById("show").style.display = "none";
+    document.getElementById("show_2").style.display = "none";
+    document.getElementById("show_3").style.display = "none";
+}
